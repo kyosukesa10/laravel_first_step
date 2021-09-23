dbbc8202a428e05adcc313c8a9ebf88e140bee67
kyosuke-sat0 <kyosuke.sato@zealot.co.jp>
2021-09-23 21:40:52 +0900

    2-2コントローラの利用 p55

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SingleActionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', [HelloController::class, 'index']);
-Route::get('/hello/other', [HelloController::class, 'other']);
 
+Route::get('/single_action', SingleActionController::class);
